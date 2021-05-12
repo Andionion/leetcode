35faec931bd43d611d282af44dc11abf5da7df7a
brody <947431990@qq.com>
2021-05-13 00:12:10 +0800

    复习第四题，二分法，查找两个数组中位数，小的那个数组舍去一半的数字

--- src/main/java/org/brody/leetcode/$0004_FindMedianSortedArrays.java
+++ src/main/java/org/brody/leetcode/$0004_FindMedianSortedArrays.java
@@ -24,42 +24,54 @@ package org.brody.leetcode;
  * @author CYF
  */
 public class $0004_FindMedianSortedArrays {
-	//基本思路是比较两个数组中第k小的数, 小的那组的前k/2个数可以直接排除, 一直到k=1或者其中某个数组排除完.
-	//前者所求数为两个数组中此时最小的那个数, 后者为长的那个数组的第k个数字
 
-	public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
-		int n = nums1.length;
-		int m = nums2.length;
-		int left = (n + m + 1) / 2;
-		int right = (n + m + 2) / 2;
-		//将偶数和奇数的情况合并,如果是奇数,会求两次同样的 k
-		return (getKth(nums1, 0, n - 1, nums2, 0, m - 1, left) + getKth(nums1, 0, n - 1, nums2, 0, m - 1, right)) * 0.5;
-	}
+    public static void main(String[] args) {
+        int[] nums1 = {1, 3, 5, 6};
+        int[] nums2 = {4, 7, 8};
 
-	private static int getKth(int[] nums1, int start1, int end1, int[] nums2, int start2, int end2, int k) {
-		int len1 = end1 - start1 + 1;
-		int len2 = end2 - start2 + 1;
+        double medianSortedArrays = findMedianSortedArrays(nums1, nums2);
+        System.out.println(medianSortedArrays);
+    }
 
-		//让len1的长度小于len2,这样就能保证如果有数组空了, 一定是len1
-		if (len1 > len2) {
-			return getKth(nums2, start2, end2, nums1, start1, end1, k);
-		}
-		//当第一个数组为空的时候, 跳出递归, 直接返回第二个数组特定值即可
-		if (len1 == 0) {
-			return nums2[start2 + k - 1];
-		}
-		//当k=1的时候, 跳出递归, 返回第一个数组和第二个数组最小的那个值
-		if (k == 1) {
-			return Math.min(nums1[start1], nums2[start2]);
-		}
+    /**
+     * 基本思路是比较两个数组中第k小的数, 小的那组的前k/2个数可以直接排除, 一直到k=1或者其中某个数组排除完.
+     *
+     * @param nums1
+     * @param nums2
+     * @return
+     */
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n = nums1.length;
+        int m = nums2.length;
+        int left = (n + m + 1) / 2;
+        int right = (n + m + 2) / 2;
+        //将偶数和奇数的情况合并,如果是奇数,会求两次同样的 k
+        return (getKth(nums1, 0, nums2, 0, left) + getKth(nums1, 0, nums2, 0, right)) * 0.5;
+    }
 
-		//比较两个数组的第 k/2 个数字, 哪个数字小, 就去掉哪个数组的前k/2个数字, 然后继续递归
-		int i = start1 + Math.min(len1, k / 2) - 1;
-		int j = start2 + Math.min(len2, k / 2) - 1;
-		if (nums1[i] > nums2[j]) {
-			return getKth(nums1, start1, end1, nums2, j + 1, end2, k - (j - start2 + 1));
-		} else {
-			return getKth(nums1, i + 1, end1, nums2, start2, end2, k - (i - start1 + 1));
-		}
-	}
+    /**
+     * @param nums1 数组 1
+     * @param i     nums1 的起始位置
+     * @param nums2 数组 2
+     * @param j     nums2 的起始位置
+     * @param k     中位数
+     * @return
+     */
+    private static int getKth(int[] nums1, int i, int[] nums2, int j, int k) {
+        if (i >= nums1.length) {
+            // nums1 为空数组
+            return nums2[j + k - 1];
+        }
+        if (j >= nums2.length) {
+            // nums2 为空数组
+            return nums1[i + k - 1];
+        }
+        if (1 == k) {
+            return Math.min(nums1[i], nums2[j]);
+        }
+        int midVal1 = i + k / 2 - 1 < nums1.length ? nums1[i + k / 2 - 1] : Integer.MAX_VALUE;
+        int midVal2 = j + k / 2 - 1 < nums2.length ? nums2[j + k / 2 - 1] : Integer.MAX_VALUE;
+        // 比较两个数组中位数大小，把小的那个数组的前 k/2 个数字去除
+        return midVal1 < midVal2 ? getKth(nums1, i + k / 2, nums2, j, k - k / 2) : getKth(nums1, i, nums2, j + k / 2, k - k / 2);
+    }
 }
