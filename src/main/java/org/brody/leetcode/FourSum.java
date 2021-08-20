7bbbeeb2e70f4cbb493a3637bd15f9f9a98e66bc
brody <947431990@qq.com>
2021-08-20 09:13:09 +0800

    双指针妙用

--- src/main/java/org/brody/leetcode/FourSum.java
+++ src/main/java/org/brody/leetcode/FourSum.java
@@ -1,5 +1,7 @@
 package org.brody.leetcode;
 
+import com.alibaba.fastjson.JSONObject;
+
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
@@ -30,51 +32,57 @@ import java.util.List;
  *
  * @author CYF
  */
-public class $0018_FourSum {
-    public static List<List<Integer>> fourSum(int[] nums, int target) {
-        //使用双循环固定两个数，用双指针找另外两个数，通过比较与target的大小，移动指针
-        List<List<Integer>> res = new LinkedList<>();
-        Arrays.sort(nums);
+public class FourSum {
+    public static void main(String[] args) {
+        int[] nums = {1, 0, -1, 0, -2, 2};
+        int target = 0;
+        List<List<Integer>> lists = fourSum(nums, target);
+        System.out.println(JSONObject.toJSONString(lists));
+    }
 
-        int n = nums.length;
-        for (int i = 0; i < n - 3; i++) {
-            //如果当前数字和上一个数字相同，跳过
+    public static List<List<Integer>> fourSum(int[] nums, int target) {
+        // 使用双循环固定两个数，用双指针找另外两个数，通过比较与target的大小，移动指针
+        List<List<Integer>> res = new LinkedList<>();
+        // 先排序，排序之后，使用双指针固定两个数，移动两个数字
+        Arrays.sort(nums);
+        int length = nums.length;
+        for (int i = 0; i < length - 3; i++) {
+            //  如果当前数字和上一个数字相同，跳过
             if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
-            //如果四个连续数字已经大于target，说明不会再有下一个了，跳出循环
+            // 如果四个连续数字已经大于target，说明不会再有下一个了，跳出循环
             if (nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target) {
                 break;
             }
-            //如果当前数字加上最后三个数字小于target，说明当前数字太小，跳出循环
-            if (nums[i] + nums[n - 1] + nums[n - 2] + nums[n - 3] < target) {
+            // 如果当前数字加上最后三个数字小于target，说明当前数字太小，跳出循环
+            if (nums[i] + nums[length - 1] + nums[length - 2] + nums[length - 3] < target) {
                 continue;
             }
-            //固定第二个数字
-            for (int j = i + 1; j < n - 2; j++) {
-                //同样的，当前数字如果等于前一个数字，就跳过
+            // 固定第二个数字
+            for (int j = i + 1; j < length - 2; j++) {
+                // 同样的，当前数字如果等于前一个数字，就跳过
                 if (j - i > 1 && nums[j] == nums[j - 1]) {
                     continue;
                 }
-                //如果当前数字加上第二个数字和它之后的两个数字之和大于target，就跳出循环
+                // 如果当前数字加上第二个数字和它之后的两个数字之和大于target，就跳出循环
                 if (nums[i] + nums[j] + nums[j + 1] + nums[j + 2] > target) {
                     break;
                 }
-                //如果当前数字加上第二个数字和最后两个个数字小于target，说明当前数字太小，跳出循环
-                if (nums[i] + nums[j] + nums[n - 1] + nums[n - 2] < target) {
+                // 如果当前数字加上第二个数字和最后两个个数字小于target，说明当前数字太小，跳出循环
+                if (nums[i] + nums[j] + nums[length - 1] + nums[length - 2] < target) {
                     continue;
                 }
-
-                //设左指针和右指针，左指针指向j+1，右指针指向n-1
+                // 设左指针和右指针，左指针指向j+1，右指针指向n-1
                 int left = j + 1;
-                int right = n - 1;
+                int right = length - 1;
                 while (left < right) {
                     int temp = nums[i] + nums[j] + nums[left] + nums[right];
-                    //如果当前四个数字之和等于target，说明找到四个
+                    // 如果当前四个数字之和等于target，说明找到四个
                     if (temp == target) {
-                        List<Integer> integerList = new LinkedList<>(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+                        List<Integer> integerList = Arrays.asList(nums[i], nums[j], nums[left], nums[right]);
                         res.add(integerList);
-                        //更新left和right
+                        // 更新重复的left和right
                         while (left < right && nums[left] == nums[left + 1]) {
                             left++;
                         }
