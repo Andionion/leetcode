ec28cb785b458090dea6898be869bbdc0f2ba13f
brody <947431990@qq.com>
2021-08-22 10:02:00 +0800

    合并两个有序链表

---
+++ src/main/java/org/brody/leetcode/MergeTwoSortedLists.java
@@ -0,0 +1,64 @@
+package org.brody.leetcode;
+
+import cn.hutool.json.JSONUtil;
+import com.alibaba.fastjson.JSONObject;
+import org.brody.leetcode.base.ListNode;
+
+/**
+ * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：l1 = [1,2,4], l2 = [1,3,4]
+ * 输出：[1,1,2,3,4,4]
+ * 示例 2：
+ * <p>
+ * 输入：l1 = [], l2 = []
+ * 输出：[]
+ * 示例 3：
+ * <p>
+ * 输入：l1 = [], l2 = [0]
+ * 输出：[0]
+ *
+ * <p>
+ * 提示：
+ * <p>
+ * 两个链表的节点数目范围是 [0, 50]
+ * -100 <= Node.val <= 100
+ * l1 和 l2 均按 非递减顺序 排列
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/merge-two-sorted-lists
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author CYF
+ */
+public class MergeTwoSortedLists {
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+        ListNode l2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+        MergeTwoSortedLists mergeTwoSortedLists = new MergeTwoSortedLists();
+        ListNode listNode = mergeTwoSortedLists.mergeTwoLists(l1, l2);
+        System.out.println(JSONUtil.formatJsonStr(JSONObject.toJSONString(listNode)));
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+
+        ListNode head;
+        if (l1.val <= l2.val) {
+            head = l1;
+            head.next = mergeTwoLists(l1.next, l2);
+        } else {
+            head = l2;
+            head.next = mergeTwoLists(l1, l2.next);
+        }
+        return head;
+    }
+}
